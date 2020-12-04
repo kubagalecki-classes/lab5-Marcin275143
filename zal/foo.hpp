4cb1d627bb6e09371cd39c10cd4ca421d538a7b6
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2020-12-05 00:38:58 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -2,11 +2,47 @@
 
 #include "Human.hpp"
 
+#include <iostream>
+#include <string>
 #include <list>
 #include <vector>
 
+using namespace std;
+
 std::vector< char > foo(std::list< Human >& people)
+    
 {
-    // Twoja implementacja tutaj
-    return {};
+       
+    string imie;
+    unsigned int wiek;
+    bool pies;
+    bool kot;
+
+    char Tak = 'y';
+    char Nie = 'n';
+
+    Human czlowiek = Human(imie, wiek, pies, kot);
+
+    char znak = 'y';
+    int rozmiar = lista.size();
+    int element_listy = 0;
+
+    vector <char> Preferencje; //(rozmiar, znak);
+    Preferencje.resize(rozmiar);
+    vector <char>::reverse_iterator it_wektor = Preferencje.rbegin();
+
+    char biezacy_znak;
+    lista.reverse();
+
+    for (list <Human>::iterator it_lista = lista.begin(); it_lista != lista.end(); it_lista++) 
+    { 
+       if (it_lista->isMonster() != true) biezacy_znak = Tak;
+       else biezacy_znak = Nie;
+       Preferencje[element_listy] = biezacy_znak;
+       it_lista->birthday();
+       element_listy++;
+    }
+
+    return (Preferencje);
+    
 }
